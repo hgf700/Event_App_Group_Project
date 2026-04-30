555b90cb4e9de0558ce045cbbdff7670ba513629
hgf700 <148117806+hgf700@users.noreply.github.com>
2026-04-30 21:35:25 +0200

    move files in frontend

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { appConfig } from './app/base/app.config';
+import { App } from './app/base/app';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
