8a192ee729142fc52101c08c2e14657c416bb466
hgf700 <148117806+hgf700@users.noreply.github.com>
2026-04-30 21:55:46 +0200

    feat: rooting to home view

--- Frontend/src/app/base/app.routes.ts
+++ Frontend/src/app/base/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// import { CreateEventComponent } from '../Views/create-event/create-event.component';
+import { Home } from '../Views/home/home';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: '', component: Home },
+    // { path: 'create-event', component: CreateEventComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
