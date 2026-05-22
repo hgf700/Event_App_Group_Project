db10fd2a34e82891c88bdd8e2925904d372a8660
hgf700 <148117806+hgf700@users.noreply.github.com>
2026-05-22 18:38:41 +0200

    remove old project

--- WebApplication1/Models/ViewModel/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication1.Models.ViewModel
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
